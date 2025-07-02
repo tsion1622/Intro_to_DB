731fe279ebccdab5e5db72d8588ca7761647dae1
tsion1622 <tayu.16.enye@gmail.com>
2025-07-02 10:23:31 +0300

    Complete Discovering Databases project with all mandatory files

---
+++ oop/Intro_to_DB/MySQLServer.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',       # change if your username is different
+        password=''        # add your password here
+    )
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
